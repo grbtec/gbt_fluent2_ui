7ba4151cac5f43ea4aa14ef15e0d400644266c7d
Railson Ferreira <44143683+railson-ferreira@users.noreply.github.com>
2025-02-26 14:22:35 -0300

    chore: update scripts

--- remove_private_dependencies.dart
+++ remove_private_dependencies.dart
@@ -5,23 +5,35 @@ void main() async {
   final content = file.readAsStringSync();
   file.writeAsString(
     content.replaceAll(
-        RegExp(".+:(?:[^:]|[\n])+hosted: (?:.|[\n])+version: .+\..+\..+"), ""),
+      RegExp(".+:(?:[^:]|[\n])+hosted: (?:.|[\n])+version: .+\..+\..+"),
+      "",
+    ),
   );
 
   final file2 = File("example/pubspec.yaml");
   final content2 = file2.readAsStringSync();
   file2.writeAsString(
     content2.replaceAll(
-        RegExp(".+:(?:[^:]|[\n])+hosted: .+:\/\/[^:]+version: .+\..+\..+"), ""),
+      RegExp(".+:(?:[^:]|[\n])+hosted: .+:\/\/[^:]+version: .+\..+\..+"),
+      "",
+    ),
   );
 
   await Future.delayed(Duration(seconds: 1));
 
-  final result1 = Process.runSync("git",["add", "."]);
+  final result1 = Process.runSync("git", ["add", "."]);
   print(result1.stdout);
   print(result1.stderr);
 
-  final result2 = Process.runSync("git",["commit", "-m", "build: remove private dependencies from pubspec.yaml and example/pubspec.yaml"]);
+  final result2 = Process.runSync(
+    "git",
+    [
+      "commit",
+      "-m",
+      "build: remove private dependencies from pubspec.yaml and example/pubspec.yaml",
+    ],
+    environment: {"GIT_AUTHOR_NAME": "CI Bot", "GIT_AUTHOR_EMAIL": "ci_bot@example.com"},
+  );
   print(result2.stdout);
   print(result2.stderr);
 }
