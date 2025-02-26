4a0ef6ac32ab55b5d84156b3465b3ae0003d7543
Railson Ferreira <44143683+railson-ferreira@users.noreply.github.com>
2025-02-26 12:41:00 -0300

    chore: update scripts

--- remove_private_dependencies.dart
+++ remove_private_dependencies.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-void main() {
+void main() async {
   final file = File("pubspec.yaml");
   final content = file.readAsStringSync();
   file.writeAsString(
@@ -14,4 +14,14 @@ void main() {
     content2.replaceAll(
         RegExp(".+:(?:[^:]|[\n])+hosted: .+:\/\/[^:]+version: .+\..+\..+"), ""),
   );
+
+  await Future.delayed(Duration(seconds: 1));
+
+  final result1 = Process.runSync("git",["add", "."]);
+  print(result1.stdout);
+  print(result1.stderr);
+
+  final result2 = Process.runSync("git",["commit", "-m", "build: remove private dependencies from pubspec.yaml and example/pubspec.yaml"]);
+  print(result2.stdout);
+  print(result2.stderr);
 }
